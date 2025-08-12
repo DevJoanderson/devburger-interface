3afc63bdccfa382ccce29c70c78bfe7038992cd1
DevJoanderson <joandersonsouza410@gmail.com>
2025-08-11 22:45:03 -0300

    feat: criação do footer

---
+++ src/components/Footer/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+ background-color: #5C2669;
+ height: 50px;
+ width: 100vw;
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ display: center;
+
+ p {
+    color: #fff;
+    font-size: 14px;
+    font-weight: align;
+    
+    
+ }
+`;

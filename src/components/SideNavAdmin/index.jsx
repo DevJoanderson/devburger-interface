7112d0de1e806957e8743415b0ac11d915283d83
DevJoanderson <joandersonsouza410@gmail.com>
2025-08-29 06:02:40 -0300

    feat: Criando o Menu Lateral

--- src/components/SideNavAdmin/index.jsx
+++ src/components/SideNavAdmin/index.jsx
@@ -1,8 +1,28 @@
+import { navLinks } from "./navLinks";
+import Logo from '../../assets/logo.svg';
+import { Container, NavLinkContainer, NavLink, Footer } from './styles';
+import { useUser } from '../../hooks/UserContext';
+import { SignOut } from "@phosphor-icons/react";
 export function SideNavAdmin() {
-
+    const { logaut } = useUser();
     return (
-        <div>
-            <h3>Menu</h3>
-        </div>
+        <Container>
+            <img src={Logo} alt="Hamburger Logo DevBurger" />
+            <NavLinkContainer>
+                {navLinks.map(link => (
+                    <NavLink key={link.id} to={link.path}>
+                        {link.icon}
+                        <span>{link.label}</span>
+                    </NavLink>
+                ))}
+
+            </NavLinkContainer>
+            <Footer>
+                <NavLink to='/login' onClick={logaut}>
+                    <SignOut />
+                    <span>Sair</span>
+                </NavLink>
+            </Footer>
+        </Container>
     )
 }
